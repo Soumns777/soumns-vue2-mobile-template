d45db875ad3c780ee453cfb0a680d84e04988c33
soumns <2794122209@qq.com>
2022-03-14 10:38:24 +0800

    :heavy_plus_sign: chore:add-Vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 Vue.config.productionTip = false
 
 import iphoneXBottom from '@/mixins/iphoneXBottomSetAside'
