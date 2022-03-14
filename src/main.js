7a6d2b4377984b169c87e609f1c5fffa3614d7a5
soumns <2794122209@qq.com>
2022-03-14 10:31:02 +0800

    ➕chore:add-Vconsole

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import iphoneXBottom from '@/mixins/iphoneXBottomSetAside'
 // mixin处理iphone的底部
 Vue.mixin(iphoneXBottom)
 
+// 引入Vconsole
+import Vconsole from 'vconsole'
+const vConsole = new Vconsole()
+Vue.use(vConsole)
+
 new Vue({
   router,
   store,
