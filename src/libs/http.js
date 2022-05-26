6a0e86ad1925ac72121016c7408ecb7faf564a5d
soumns <2794122209@qq.com>
2022-05-26 17:46:36 +0800

    ✨ feat:soumns-request

--- src/libs/http.js
+++ src/libs/http.js
@@ -1,4 +1,23 @@
 import axios from 'axios'
+import { CancelToken,isCancel } from 'axios';
+
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    axios.defaults.baseURL = 'http://api.zhufeng.cn',
+    break;
+    case 'test':
+    axios.defaults.baseURL = 'http://192.168.20.12:8080',
+    break;
+
+  default:
+    axios.defaults.baseURL = 'http://127.0.0.1:3000',
+}
+
+// 取消请求
+let cancal;
+
+
 
 /*
  * create instance
@@ -16,7 +35,11 @@ const instance = axios.create({
  */
 instance.interceptors.request.use((config) => {
   // doing something before request
-  console.log(config, '--->config')
+  if(cancal) cancal()
+  config.cancelToken = new CancelToken( (c)=> {
+      cancal = c
+  })
+
   return config
 }),
   (error) => {
@@ -34,8 +57,14 @@ instance.interceptors.response.use((response) => {
 }),
   (error) => {
     // doing something when response error
-    console.log(error, '--->报错提示')
-    return Promise.reject(error)
+
+    if(isCancel(error)) {
+  console.log(error.message);
+    }else {
+      return Promise.reject(error)
+    }
+
+
   }
 
 export { instance }
